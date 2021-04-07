a29ffb36eee38ebb6a3e8d8b48699eaa826bdad6
Brooklyn Hairston <bhairston@my365.bellevue.edu>
2021-04-07 18:06:37 -0400

    updating

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended': true}));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../dist/nodebucket')));
-app.use('/', express.static(path.join(__dirname, '../dist/nodebucket')));
+app.use('', express.static(path.join(__dirname, '../dist/nodebucket')));
 
 /**
  * Variables
